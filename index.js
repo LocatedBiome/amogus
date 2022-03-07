b94704219a4743dbe7c538baa765bc62d0396fe3
LocatedReaper <82606980+LocatedBiome@users.noreply.github.com>
2022-03-07 14:36:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+if (message.content.startsWith("SUS") {
+    message.channel.send("the imposter is looking kinda phat");
+    };
+if (message.content.startsWith("nothing") {
+    return null;
+    };
